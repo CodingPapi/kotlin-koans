73cba6cda7f71df81bc9b47513c194de74b2f34b
CodingPapi <codingpapi@gmail.com>
2015-12-23 07:46:24 +0800

    Pass 17th testcase

--- src/ii_collections/_17_MaxMin.kt
+++ src/ii_collections/_17_MaxMin.kt
@@ -5,12 +5,22 @@ fun example4() {
     val longestString = listOf("a", "b").maxBy { it.length }
 }
 
+val Customer.costOfOrders: Double get() {
+    return this.orders.flatMap { it.products }.map { it.price }.sum()
+}
+
 fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? {
     // Return a customer whose order count is the highest among all customers
-    todoCollectionTask()
+//    todoCollectionTask()
+    // TODO,cannot be reduced ,finger this out later
+    //    return this.customers.maxBy { it.orderedProducts.map { it.price }.reduce { product1, product2 -> product1 + product2  } }
+    return this.customers.maxBy { it.costOfOrders }
+//    return this.customers[0].costOfOrders
+
 }
 
 fun Customer.getMostExpensiveOrderedProduct(): Product? {
     // Return the most expensive product which has been ordered
-    todoCollectionTask()
+//    todoCollectionTask()
+    return this.orders.flatMap { it.products }.maxBy { it.price }
 }
